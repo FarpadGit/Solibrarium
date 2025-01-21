8031edc5eb15b8d4b6bc79891bc447de30517135
FarpadGit <farpadmail@gmail.com>
2025-01-21 19:36:28 +0100

    Create CachedBooks.js

---
+++ utils/CachedBooks.js
@@ -0,0 +1,10 @@
+import redis from "@/utils/RedisConnect";
+
+// The Google Books API has a daily limit of 1000 requests so it would be prudent to cache any books that were already queried
+export async function cacheBook(book) {
+    await redis.set(book.id, JSON.stringify(book));
+}
+
+export async function getFromCache(id) {    
+    return await redis.get(id);
+}
