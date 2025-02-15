fdc3a3eb3c90c0ae77b01fdee79fd40cfc44d9ba
FarpadGit <farpadmail@gmail.com>
2025-02-16 00:31:43 +0100

    Create FilterButton.jsx

---
+++ components/header/FilterButton.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/Popover";
+import { AnimatePresence } from "framer-motion";
+import dynamic from "next/dynamic";
+const FiltersModal = dynamic(
+  () => import("@/components/popovers/FiltersModal").then((res) => res.default),
+  { ssr: false }
+);
+
+export default function FilterButton() {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <button type="button" className="filters_btn header_icon_btn">
+          <Image
+            src="/icons/filter.svg"
+            alt="Filter"
+            width={20}
+            height={20}
+            className="md:w-[30px] md:h-[30px]"
+          />
+        </button>
+      </PopoverTrigger>
+      <PopoverContent align="center" side="left">
+        <AnimatePresence>
+          <FiltersModal />
+        </AnimatePresence>
+      </PopoverContent>
+    </Popover>
+  );
+}
