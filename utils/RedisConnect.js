4a7da9f3050e57fc6bc62ebb788b559607e293c8
FarpadGit <farpadmail@gmail.com>
2025-04-26 19:29:26 +0200

    Update RedisConnect.js

--- utils/RedisConnect.js
+++ utils/RedisConnect.js
@@ -3,6 +3,7 @@ import { Redis } from '@upstash/redis';
 const options = {
     url: process.env.KV_REST_API_URL,
     token: process.env.KV_REST_API_TOKEN,
+    cache: "default"
   }
   
 const redis = new Redis(options);
