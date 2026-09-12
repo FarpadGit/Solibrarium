67264350f051e8014bca8af94d94d0a65db52b01
FarpadGit <farpadmail@gmail.com>
2026-09-12 21:24:40 +0200

    Update layout.jsx

--- app/(home)/layout.jsx
+++ app/(home)/layout.jsx
@@ -1,6 +1,6 @@
 export default function HomeLayout({ bestsellers, bookreviews }) {
   return (
-    <div className="w-[calc(100vw-2*var(--page-margin))]">
+    <div className="w-[calc(100vw-2*var(--page-margin))] min-[1792px]:w-[92rem]">
       {bestsellers}
       {bookreviews}
     </div>
